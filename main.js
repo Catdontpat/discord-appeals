21c328e6cbd8358497558c55a22ed82d4f5ba19a
Cattu(Justin) <KirbyXstar@gmail.com>
2024-10-22 05:58:50 -0500

    More base changes

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const path = require('path');
+const fs = require('fs')
 // Main Discord.js Client Implentations & Base express imports
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Events, GatewayIntentBits, REST, Collection } = require('discord.js');
 require('dotenv').config();
 const token = require(process.env.TOKEN)
 const clientID = require(process.env.CLIENTID)
@@ -18,3 +19,29 @@ mongoose.connect(process.env.MONGO_URL, {
     useUnifiedTopology: true,
     useNewUrlParser: true
 }).then(console.log(`≽^•⩊•^≼ | Connected to mongoDB!`))
+
+// Command handler for seperate command files
+const rest = new REST({ version: '10' }).setToken(token)
+client.commands = new Collection()
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+const commands = [];
+
+for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const filePath = path.join(commandsPath, file);
+        const command = require(filePath);
+        if ('data' in command && 'execute' in command) {
+            client.commands.set(command.data.name, command);
+            commands.push(command.data.toJSON());
+        } else {
+            console.log(`[WARNING] | The following command in ${filePath} is missing data or proper executing properties to run`);
+        }
+    }
+}
+
+// Event handler for reading seperate files for each event that occurs
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
